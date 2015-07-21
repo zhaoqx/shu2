5974407953a54544e96e9489786281926c10034b
zhaoxin2 <zhaoqx@outlook.com>
2015-07-21 17:23:05 +0800

    新建项目

---
+++ Includes/simplecms-config.php
@@ -0,0 +1,9 @@
+<?php
+    define('DB_NAME', 'simplecms341');
+    define('DB_USER', 'simplecmsuser565');
+    define('DB_PASSWORD', 'DwS?gfb4KzV&');
+    define('DB_HOST', 'localhost');
+
+    define('DEFAULT_ADMIN_USERNAME', 'admin');
+    define('DEFAULT_ADMIN_PASSWORD', 'shuzhaoxin');
+?>
